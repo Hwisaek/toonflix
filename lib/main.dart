d10b6cb5982ce2fa80331865ca43f1a42827e784
Hwisaek <dia_changmin@naver.com>
2023-04-20 22:01:46 +0900

    toonflix done

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  List<int> numbers = [];
-
-  void onClicked() {
-    setState(() {
-      numbers.add(numbers.length);
-      print(numbers);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.amber,
-        body: Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Click Count",
-                  style: TextStyle(fontSize: 30),
-                ),
-                for (var n in numbers) Text('$n'),
-                IconButton(
-                    onPressed: onClicked,
-                    icon: const Icon(Icons.add_box_rounded))
-              ],
-            ),
-          ),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
